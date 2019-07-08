fb25b2d9088166a54ba17fb2caaf3ee59b858be5
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-07-08 14:41:13 +0200

    updated files

--- get_next_line.c
+++ get_next_line.c
@@ -6,60 +6,66 @@
 /*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 15:21:23 by pmalope           #+#    #+#             */
-/*   Updated: 2019/07/03 15:53:30 by pmalope          ###   ########.fr       */
+/*   Updated: 2019/07/06 11:32:43 by pmalope          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_func(int index, char **tab)
+static int	ft_find_newline(int pos, char **tab)
 {
 	int	i;
 
 	i = 0;
-	while (tab[index][i] != '\n' && tab[index][i] != '\0')
+	while (tab[pos][i] != '\n' && tab[pos][i] != '\0')
 		i++;
 	return (i);
 }
 
-static int	ft_new_line(int fd, char **line, char **tab, int line_size)
+static int	ft_next_line(int fd_pos, char **line, char **tab, int length)
 {
-	int		len;
+	int		res;
+	int		index;
 	char	*ptr;
 
-	len = ft_func(fd, tab);
-	if (tab[fd][len] == '\n')
+	index = ft_find_newline(fd_pos, tab);
+	if (tab[fd_pos][index] == '\n')
 	{
-		*line = ft_strsub(tab[fd], 0, len);
-		ptr = ft_strdup(tab[fd] + len + 1);
-		ft_strdel(&tab[fd]);
-		tab[fd] = ptr;
-		if (tab[fd][0] == '\0')
-			ft_strdel(&tab[fd]);
+		*line = ft_strsub(tab[fd_pos], 0, index);
+		ptr = ft_strdup(tab[fd_pos] + index + 1);
+		ft_strdel(&tab[fd_pos]);
+		tab[fd_pos] = ptr;
+		if (tab[fd_pos][0] == '\0')
+			ft_strdel(&tab[fd_pos]);
 	}
-	else if (tab[fd][len] == '\0')
+	else if (tab[fd_pos][index] == '\0')
 	{
-		if (line_size == BUFF_SIZE)
-			return (get_next_line(fd, line));
-		*line = ft_strdup(tab[fd]);
-		ft_strdel(&tab[fd]);
+		if (length == BUFF_SIZE)
+		{
+			res = get_next_line(fd_pos, line);
+			return (res);
+		}
+		*line = ft_strdup(tab[fd_pos]);
+		ft_strdel(&tab[fd_pos]);
 	}
 	return (1);
 }
 
 static int	ft_read_line(int fd, char *buf, char **tab)
 {
+	int		pos;
 	int		len;
-	char	*join_buf;
+	char	*ptr;
 
+	pos = fd;
+	if (tab[pos] == NULL)
+		tab[pos] = ft_strnew(1);
 	while ((len = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[len] = '\0';
-		if (tab[fd] == NULL)
-			tab[fd] = ft_strnew(1);
-		join_buf = ft_strjoin(tab[fd], buf);
-		free(tab[fd]);
-		tab[fd] = join_buf;
+		ptr = ft_strjoin(tab[pos], buf);
+		free(tab[pos]);
+		tab[pos] = ptr;
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
@@ -73,12 +79,12 @@ int			get_next_line(const int fd, char **line)
 	static char	*tmp_buf[1024];
 	char		buffer[BUFF_SIZE + 1];
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || read(fd, NULL, 0) == -1)
 		return (-1);
 	if ((n = ft_read_line(fd, buffer, tmp_buf)) < 0)
 		return (-1);
-	else if (n == 0 && (tmp_buf[fd] == NULL || tmp_buf[fd][0] == '\0'))
+	if (n == 0 && (tmp_buf[fd] == NULL || tmp_buf[fd][0] == '\0'))
 		return (0);
-	res = ft_new_line(fd, line, tmp_buf, n);
+	res = ft_next_line(fd, line, tmp_buf, n);
 	return (res);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: pmalope <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 15:18:48 by pmalope           #+#    #+#             */
-/*   Updated: 2019/07/03 15:54:13 by pmalope          ###   ########.fr       */
+/*   Updated: 2019/07/06 11:32:28 by pmalope          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
-
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <sys/uio.h>
 # include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
